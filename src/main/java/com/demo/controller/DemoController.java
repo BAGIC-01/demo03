ada2bed6b5d6275160905713faa31a95006cf3c8
developer-bagic-02 <ketanexam92@gmail.com>
2024-06-13 15:29:21 +0530

    Update DemoController.java

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -10,7 +10,7 @@ public class DemoController {
 	
 	@GetMapping("/cities")
 	public String getCities () {
-		return "mumbai,pune,banglore,Delhi,Hyderabad,Nagpur";
+		return "mumbai,pune,banglore,Delhi,Hyderabad,Nagpur,Pandharpur";
 	}
 
 	@GetMapping("/contries")
