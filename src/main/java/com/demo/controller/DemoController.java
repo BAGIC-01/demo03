1d8c0ac18ee8237695cc32a1dcdb1eda0dd09f02
ketan922292 <ketanthombare.drive@gmail.com>
2024-06-14 06:25:23 +0000

    fd

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -15,7 +15,7 @@ public class DemoController {
 
 	@GetMapping("/contries")
 	public String getContries () {
-		return "India,Japan,China,America,Peru,Canada,Pakistan,afganistan,taiwan,Russia";
+		return "India,Japan,China,America,Peru,Canada,Pakistan,afganistan,taiwan,Russia,antartica";
 	}
 
 	
