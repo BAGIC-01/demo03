539a90adec9ef2f38bc1a3a60e1c26331f5fce9d
ketan922292 <ketanthombare.drive@gmail.com>
2024-06-14 15:06:01 +0530

    Update DemoController.java

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -10,7 +10,7 @@ public class DemoController {
 	
 	@GetMapping("/cities")
 	public String getCities () {
-		return "mumbai,pune,banglore,Delhi,Hyderabad,Nagpur,Pandharpur,satara,shivajainagar,sambhajinagar,ulhasnagar,panvel,Khopili,dombivali,Airoli,Rabale";
+		return "mumbai,pune,banglore,Delhi,Hyderabad,Nagpur,Pandharpur,satara,shivajainagar,sambhajinagar,ulhasnagar,panvel,Khopili,dombivali,Airoli,Rabale,koparkhairane";
 	}
 
 	@GetMapping("/contries")
