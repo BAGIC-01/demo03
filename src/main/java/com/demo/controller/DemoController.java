d0ef27a95a28c90dc9512692c02fa3db9c57a373
ketan922292 <ketanthombare.drive@gmail.com>
2024-06-14 05:57:34 +0000

    added new files

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -15,7 +15,7 @@ public class DemoController {
 
 	@GetMapping("/contries")
 	public String getContries () {
-		return "India,Japan,China,America";
+		return "India,Japan,China,America,Peru";
 	}
 
 	
